8127aac211e650d62645dd0afe56acfc2edd444b
MaciejFigat <60787137+MaciejFigat@users.noreply.github.com>
2021-08-29 00:36:53 +0200

    minor things changed in the server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,19 @@
 import express from 'express'
+import dotenv from 'dotenv'
+import colors from 'colors'
+// import connectDB from '../config/db.ts'
+
+dotenv.config()
+// connectDB()
 
 const app = express()
+app.use(express.json())
 
+const PORT = process.env.PORT || 5000
 
+app.listen(PORT, () => {
+    console.log(
+        colors.yellow.bold(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 
-app.listen(3001, () => {
-    console.log('it is on!')
+    )
 })
