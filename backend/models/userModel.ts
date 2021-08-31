4af4c78752da5a59583bd9d6cd85e3adfe761184
MaciejFigat <60787137+MaciejFigat@users.noreply.github.com>
2021-08-31 23:00:07 +0200

    added userModel in models, significant changes in moongose 6.0

---
+++ backend/models/userModel.ts
@@ -0,0 +1,55 @@
+import { model, Schema } from "mongoose"
+import bcrypt from 'bcryptjs'
+
+export interface User {
+    name: string
+    email: string
+    password: string
+    isAdmin: boolean
+
+}
+
+const userSchema = new Schema<User>(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        isAdmin: {
+            type: Boolean,
+            required: true,
+            default: false,
+        },
+    },
+    {
+        timestamps: true,
+    }
+)
+
+userSchema.methods.matchPassword = async function (enteredPassword: string) {
+    return await bcrypt.compare(enteredPassword, this.password)
+}
+
+userSchema.pre('save', async function (this: any, next: Function,) {
+    if (!this.isModified('password')) {
+        next()
+    }
+
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password, salt)
+})
+
+const User = model<User>('User', userSchema)
+
+export default User
+
+
