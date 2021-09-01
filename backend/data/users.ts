838e33939a4911b5fa650d1a35eb80ade0b819ed
MaciejFigat <60787137+MaciejFigat@users.noreply.github.com>
2021-09-01 23:49:59 +0200

    added data seeder script to populate the database, also added a folder with users I want to be present in the database

---
+++ backend/data/users.ts
@@ -0,0 +1,16 @@
+import bcrypt from 'bcryptjs'
+
+const users = [
+    {
+        name: 'Admin User',
+        email: 'admin@somethin.com',
+        password: bcrypt.hashSync('123456', 10),
+        isAdmin: true,
+    },
+    {
+        name: 'Donald T.',
+        email: 'donald@somethin.com',
+        password: bcrypt.hashSync('123456', 10),
+    }
+]
+export default users
