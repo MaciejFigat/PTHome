8b04bf5d31594a622c792fdb2d0350d3a1b5b93f
MaciejFigat <60787137+MaciejFigat@users.noreply.github.com>
2021-09-07 22:45:24 +0200

    redux toolkit setup instead of the js approach

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App'
 import smoothScrollPolyfill from 'smoothscroll-polyfill'
 import { Provider } from 'react-redux'
-import store from './store'
+import { store } from './app/store'
 import reportWebVitals from './reportWebVitals'
 import { BrowserRouter as Router } from 'react-router-dom'
 
