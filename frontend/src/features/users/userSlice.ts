9dc1388ce6a3c72d5ad6e8627e757958e014510a
MaciejFigat <60787137+MaciejFigat@users.noreply.github.com>
2021-09-14 11:55:13 +0200

    ReduxToolkit v2 remake continues

--- frontend/src/features/users/userSlice.ts
+++ frontend/src/features/users/userSlice.ts
@@ -1,4 +1,17 @@
-import { createSlice } from "@reduxjs/toolkit"
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
+import axios from 'axios'
+
+// the thunk for posting the header
+
+const fetchUserById = createAsyncThunk(
+    'user/postingCredentials',
+    async (userId, thunkAPI) => {
+        const response = await userAPI.fetchById(userId)
+        return response.data
+    }
+)
+
+
 
 const userSlice = createSlice({
     name: 'user',
@@ -8,19 +21,59 @@ const userSlice = createSlice({
         error: {}
     },
     reducers: {
-        login: (state, action) => {
+        loginUser: (state, action) => {
             state.userInfo = action.payload
-            state.loading = false
+            // state.loading = false
             state.error = action.payload
         },
-        register: (state, action) => {
+        registerUser: (state, action) => {
             state.userInfo = action.payload
-            state.loading = false
+            // state.loading = false
             state.error = action.payload
         }
     }
 })
 
 
-export const { login, register } = userSlice.actions
+export const { loginUser, registerUser } = userSlice.actions
+// export const { actions, reducer } = userSlice
 export default userSlice.reducer
+
+// export const login: any = (email: string, password: string) => async (dispatch: any) => {
+
+//     try {
+//         dispatch(
+//             actions.loginUser({ loading: true })
+//         )
+//         const config = {
+//             headers: {
+//                 'Content-Type': 'application/json',
+//             },
+//         }
+
+//         const { data } = await axios.post(
+//             '/api/users/login',
+//             { email, password },
+//             config
+//         )
+//         dispatch(
+//             actions.loginUser({ loading: false }),
+//             { payload: data, }
+
+//         )
+
+
+//         localStorage.setItem('userInfo', JSON.stringify(data))
+//     } catch (error: any) {
+//         dispatch({
+
+//             payload:
+//                 error.response && error.response.data.message
+//                     ? error.response.data.message
+//                     : error.message,
+//         },
+//             actions.loginUser({ loading: false })
+//         )
+//     }
+// }
+
