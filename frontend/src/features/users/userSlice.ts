25d04aad9ab4d3f88741f7a7c5525295dc31cc4d
MaciejFigat <60787137+MaciejFigat@users.noreply.github.com>
2021-09-16 23:25:16 +0200

    ReduxToolkit v4 remake continues

--- frontend/src/features/users/userSlice.ts
+++ frontend/src/features/users/userSlice.ts
@@ -2,40 +2,54 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
 import axios from 'axios'
 
 // the thunk for posting the header
+interface UserInfo {
+    name: string
+    email: string
+    password: string
+    isAdmin: boolean
+    token: string
 
-const fetchUserById = createAsyncThunk(
-    'user/postingCredentials',
-    async (userInfo, thunkAPI) => {
-        const config = {
-            headers: {
-                'Content-Type': 'application/json',
-            },
+}
+
+//@ts-ignore
+// { password, email },
+
+const sendUserId = (email: string, password: string) => {
+    createAsyncThunk(
+        'user/sendUser',
+
+        async (thunkAPI) => {
+            const config = {
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            }
+
+            const { data } = await axios.post(
+                '/api/users/login',
+                { email, password },
+                config
+            )
+            return data
         }
-        const { data } = await axios.post(
-            '/api/users/login',
-            { email, password },
-            config
-        )
-        return data
-    }
-)
-
+    )
+}
 
 
 const userSlice = createSlice({
-    name: 'user',
+    name: 'userLogin',
     initialState: {
         userInfo: {},
         loading: false,
         error: {}
     },
     reducers: {
-        loginUser: (state, action) => {
+        login: (state, action) => {
             state.userInfo = action.payload
             // state.loading = false
             state.error = action.payload
         },
-        registerUser: (state, action) => {
+        register: (state, action) => {
             state.userInfo = action.payload
             // state.loading = false
             state.error = action.payload
@@ -43,52 +57,15 @@ const userSlice = createSlice({
     },
     extraReducers: {
         // Add reducers for additional action types here, and handle loading state as needed
-        [fetchUserById.fulfilled]: (state, action) => {
+        [sendUserId.fulfilled]: (state, action) => {
             state.userInfo = action.payload
-        }
+        },
+
     }
 })
 // dispatch(fetchUserById(123)) 
 
-export const { loginUser, registerUser } = userSlice.actions
+export const { login, register } = userSlice.actions
 // export const { actions, reducer } = userSlice
 export default userSlice.reducer
-
-// export const login: any = (email: string, password: string) => async (dispatch: any) => {
-
-//     try {
-//         dispatch(
-//             actions.loginUser({ loading: true })
-//         )
-//         const config = {
-//             headers: {
-//                 'Content-Type': 'application/json',
-//             },
-//         }
-
-//         const { data } = await axios.post(
-//             '/api/users/login',
-//             { email, password },
-//             config
-//         )
-//         dispatch(
-//             actions.loginUser({ loading: false }),
-//             { payload: data, }
-
-//         )
-
-
-//         localStorage.setItem('userInfo', JSON.stringify(data))
-//     } catch (error: any) {
-//         dispatch({
-
-//             payload:
-//                 error.response && error.response.data.message
-//                     ? error.response.data.message
-//                     : error.message,
-//         },
-//             actions.loginUser({ loading: false })
-//         )
-//     }
-// }
 
