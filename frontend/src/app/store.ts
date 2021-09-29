d0c4985b8cc46d2372238c73a847db8b9681829c
MaciejFigat <60787137+MaciejFigat@users.noreply.github.com>
2021-09-29 15:54:27 +0200

    redux toolkit store revert - both vanilla reducers for users working

--- frontend/src/app/store.ts
+++ frontend/src/app/store.ts
@@ -1,32 +1,28 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux'
-import thunk from 'redux-thunk'
-import { composeWithDevTools } from 'redux-devtools-extension'
 import { userLoginReducer, userRegisterReducer } from '../features/users/userReducers'
+import { configureStore } from '@reduxjs/toolkit'
 
-const reducer = combineReducers({
-    userLogin: userLoginReducer,
-    userRegister: userRegisterReducer,
+
+// const userInfoFromStorage = localStorage.getItem('userInfo')
+//     //@ts-ignore
+//     ? JSON.parse(localStorage.getItem('userInfo'))
+//     : null
+
+
+// const initialState = {
+//     userLogin: { userInfo: userInfoFromStorage },
+// }
+
+
+export const store = configureStore({
+    reducer: {
+        // user: userReducer,
+        userLogin: userLoginReducer,
+        userRegister: userRegisterReducer,
+    },
 
 })
 
-const userInfoFromStorage = localStorage.getItem('userInfo')
-    //@ts-ignore
-    ? JSON.parse(localStorage.getItem('userInfo'))
-    : null
-
-
-const initialState = {
-    userLogin: { userInfo: userInfoFromStorage },
-}
-const middleware = [thunk]
-
-const store = createStore(
-    reducer,
-    initialState,
-    composeWithDevTools(applyMiddleware(...middleware))
-)
-
-// export type AppDispatch = typeof store.dispatch
-// export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>
 
 export default store
