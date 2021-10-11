c46cb28230152943a72a405513907c22d10027c3
MaciejFigat <60787137+MaciejFigat@users.noreply.github.com>
2021-10-11 16:29:55 +0200

    Form correction

--- frontend/src/styles/login.js
+++ frontend/src/styles/login.js
@@ -21,6 +21,7 @@ export const Wrapper = styled.section`
 export const Form = styled.form`
   margin: 0 auto;
   width: 100%;
+  z-index: -1;
   max-width: 414px;
   padding: 1.3rem;
   display: flex;
