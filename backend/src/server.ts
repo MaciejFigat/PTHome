9b18d571821fdc54fda04663d02f25b22794143a
MaciejFigat <60787137+MaciejFigat@users.noreply.github.com>
2021-10-18 13:58:50 +0200

    deployment version 0.0.8

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -28,12 +28,12 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '/frontend/build')))
 
     // app.get('service-worker.js', (req, res) => {
-    app.get('service-worker.js', (req, res) => {
-        res.sendFile(
-            path.resolve(__dirname, 'frontend', 'build', 'service-worker.js')
-            // path.resolve(__dirname, 'frontend', 'build')
-        )
-    })
+    // app.get('service-worker.js', (req, res) => {
+    //     res.sendFile(
+    //         path.resolve(__dirname, 'frontend', 'build', 'service-worker.js')
+    //         // path.resolve(__dirname, 'frontend', 'build')
+    //     )
+    // })
     app.get('*', (req, res) =>
         res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
     )
