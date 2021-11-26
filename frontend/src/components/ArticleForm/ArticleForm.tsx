ba0679a3f9570e6008688aa34f12e45086567ad9
MaciejFigat <60787137+MaciejFigat@users.noreply.github.com>
2021-11-26 20:24:08 +0100

    admin article manage trimming

--- frontend/src/components/ArticleForm/ArticleForm.tsx
+++ frontend/src/components/ArticleForm/ArticleForm.tsx
@@ -1,24 +1,12 @@
-import React, { useState } from 'react'
+import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { useAppDispatch, useAppSelector } from '../../app/reduxHooks'
 import {
-  createArticle,
   getArticles,
   deleteArticle,
-  editArticle,
 } from '../../features/articles/articleSlice'
 
-import {
-  ResponsiveDiv,
-  FormContainerDiv,
-  ContactFormContainer,
-  ContactFormStyled,
-  ContactField,
-  ContactFieldContent,
-  MessageField,
-  SendButtonWrapper,
-  SendButton,
-} from './ArticleForm.styled'
+import { SendButton } from './ArticleForm.styled'
 
 interface ArticleFormProps {}
 
@@ -27,46 +15,12 @@ const ArticleForm: React.FC<ArticleFormProps> = () => {
 
   const articles: any[] = useAppSelector((state) => state.article.articles)
 
-  const [topline, setTopline] = useState('topline')
-  const [headline, setHeadline] = useState('headline')
-  const [subtitle, setSubtitle] = useState('subtitle')
-  const [author, setAuthor] = useState('author')
-  const [imgLink, setImgLink] = useState('link')
-
-  const newArticleInfo = {
-    topline: topline,
-    headline: headline,
-    subtitle: subtitle,
-    author: author,
-    imgLink: imgLink,
-  }
-  // testing editArticle
-  const article = {
-    _id: '6197f2fe0855d331b0177fd9',
-    topline: 'testing Editing',
-    headline:
-      'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure, mollitia?',
-    subtitle: 'Lorem ipsum dolor sit amet consectet.',
-    imgLink: 'https://source.unsplash.com/-fkLJ9Ws5XQ',
-    author: 'James 2',
-  }
-
-  const editHandler = (e: any) => {
-    e.preventDefault()
-    dispatch(editArticle(article))
-  }
+  useEffect(() => {
+    dispatch(getArticles())
+  }, [dispatch])
 
   const deleteHandler = (id: string) => {
     dispatch(deleteArticle(id))
-  }
-
-  const createHandler = (e: any) => {
-    e.preventDefault()
-    dispatch(createArticle(newArticleInfo))
-  }
-  const getHandler = (e: any) => {
-    e.preventDefault()
-    dispatch(getArticles())
   }
 
   return (
@@ -110,65 +64,6 @@ const ArticleForm: React.FC<ArticleFormProps> = () => {
             ))}
         </tbody>
       </table>
-      <FormContainerDiv>
-        <ResponsiveDiv>
-          {' '}
-          <ContactFormContainer>
-            <ContactFormStyled>
-              <ContactField>
-                <label>Topline</label>
-                <ContactFieldContent
-                  type='text'
-                  value={topline}
-                  placeholder='Toopline!!!'
-                  onChange={(e: any) => setTopline(e.target.value)}
-                />
-              </ContactField>
-              <ContactField>
-                <label>Headline</label>
-                <ContactFieldContent
-                  type='headline'
-                  value={headline}
-                  placeholder='Headlin here'
-                  onChange={(e: any) => setHeadline(e.target.value)}
-                />
-              </ContactField>
-              <ContactField>
-                <label>Subtitle</label>
-                <MessageField
-                  as='textarea'
-                  row='8'
-                  value={subtitle}
-                  placeholder='Subtitle here'
-                  onChange={(e: any) => setSubtitle(e.target.value)}
-                ></MessageField>
-              </ContactField>
-              <ContactField>
-                <label>Author</label>
-                <ContactFieldContent
-                  value={author}
-                  placeholder='Set the author'
-                  onChange={(e: any) => setAuthor(e.target.value)}
-                ></ContactFieldContent>
-              </ContactField>
-              <ContactField>
-                <label>Img link</label>
-                <ContactFieldContent
-                  value={imgLink}
-                  placeholder='put on the lotion'
-                  onChange={(e: any) => setImgLink(e.target.value)}
-                ></ContactFieldContent>
-              </ContactField>
-              <SendButtonWrapper>
-                <SendButton onClick={getHandler}>Get Test</SendButton>
-                <SendButton onClick={editHandler}>Edit</SendButton>
-                <SendButton onClick={deleteHandler}>Delete</SendButton>
-                <SendButton onClick={createHandler}>Create</SendButton>
-              </SendButtonWrapper>
-            </ContactFormStyled>{' '}
-          </ContactFormContainer>
-        </ResponsiveDiv>
-      </FormContainerDiv>
     </>
   )
 }
