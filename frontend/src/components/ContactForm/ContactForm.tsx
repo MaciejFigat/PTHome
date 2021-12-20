6129bf847bbab40eb43045c7780ff42d1c7a76e6
MaciejFigat <60787137+MaciejFigat@users.noreply.github.com>
2021-12-21 00:17:38 +0100

    contact form v2

--- frontend/src/components/ContactForm/ContactForm.tsx
+++ frontend/src/components/ContactForm/ContactForm.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
-import Toast from '../Toast/Toast'
+import emailjs from 'emailjs-com'
+// import Toast from '../Toast/Toast'
 import {
   ResponsiveDiv,
   FormContainerDiv,
@@ -20,6 +21,12 @@ const ContactForm: React.FC<ContactFormProps> = () => {
   const [toastVersion, setToastVersion] = useState('none')
   const [toastMessage, setToastMessage] = useState('')
 
+  const messageResetHandler = (e: any) => {
+    e.preventDefault()
+    setToastVersion('reset')
+    setToastMessage('Formularz zresetowany')
+  }
+
   // EmailJS
   const EMAILJS_ID = process.env.MY_EMAILJS_ID
   const SERVICE_ID = process.env.MY_SERVICE_ID
@@ -31,7 +38,88 @@ const ContactForm: React.FC<ContactFormProps> = () => {
     message: formMessage,
     reply_to: email,
   }
+  const emailJSSendHandler = (e: any) => {
+    e.preventDefault()
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i
+    // setSpinnerVisible(true)
+    if (name.length < 1) {
+      setToastVersion('failure')
+      // setSpinnerVisible(false)
+      setToastMessage('Podaj imię')
+    } else if (!regex.test(email)) {
+      setToastVersion('failure')
+      // setSpinnerVisible(false)
+      setToastMessage('Wpisz prawidłowy email')
+    } else if (formMessage.length < 3) {
+      setToastVersion('failure')
+      // setSpinnerVisible(false)
+      setToastMessage('Wpisz wiadomość')
+    } else {
+      // @ts-ignore
+      emailjs.send(SERVICE_ID, TEMPLATE_ID, templateParams, EMAILJS_ID).then(
+        function () {
+          setToastVersion('success')
+          // setSpinnerVisible(false)
+          setToastMessage('Wiadomość wysłana!')
+        },
+        function () {
+          setToastVersion('failure')
+          // setSpinnerVisible(false)
+          setToastMessage('Nie udało się wysłać wiadomości')
+        }
+      )
+    }
+  }
+  return (
+    <>
+      <FormContainerDiv>
+        <ResponsiveDiv>
+          {' '}
+          <ContactFormContainer>
+            {/* <Loader
+              type='ThreeDots'
+              color='var(--text-main)'
+              height={20}
+              width={60}
+              visible={spinnerVisible}
+            /> */}
+            <ContactFormStyled>
+              <ContactField>
+                <label> Imię</label>
+                <ContactFieldContent
+                  type='text'
+                  value={name}
+                  placeholder='Wpisz swoje imię'
+                  onChange={(e: any) => setName(e.target.value)}
+                />
+              </ContactField>
+              <ContactField>
+                <label> Email:</label>
+                <ContactFieldContent
+                  type='email'
+                  value={email}
+                  placeholder='Wpisz email kontaktowy'
+                  onChange={(e: any) => setEmail(e.target.value)}
+                />
+              </ContactField>
+              <ContactField>
+                <label> Wiadomość:</label>
+                <MessageField
+                  value={formMessage}
+                  placeholder='Wpisz wiadomość'
+                  onChange={(e: any) => setFormMessage(e.target.value)}
+                ></MessageField>
+              </ContactField>
+              <SendButtonWrapper>
+                <SendButton onClick={emailJSSendHandler}>Wyślij</SendButton>
 
-  return <div></div>
+                <SendButton onClick={messageResetHandler}>reset</SendButton>
+              </SendButtonWrapper>
+            </ContactFormStyled>{' '}
+          </ContactFormContainer>
+        </ResponsiveDiv>
+      </FormContainerDiv>
+    </>
+  )
 }
 export default ContactForm
