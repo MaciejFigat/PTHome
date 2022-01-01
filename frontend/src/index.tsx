56b44fa022c10369f187d62ce6d6d712ed05ca3b
MaciejFigat <60787137+MaciejFigat@users.noreply.github.com>
2022-01-01 23:50:45 +0100

    deployment tryout v4.11

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import smoothScrollPolyfill from 'smoothscroll-polyfill'
+// import smoothScrollPolyfill from 'smoothscroll-polyfill'
 import { Provider } from 'react-redux'
 import { store } from './app/store'
 import reportWebVitals from './reportWebVitals'
 import { BrowserRouter as Router } from 'react-router-dom'
 
-smoothScrollPolyfill.polyfill()
+// smoothScrollPolyfill.polyfill()
 
 ReactDOM.render(
   <Provider store={store}>
