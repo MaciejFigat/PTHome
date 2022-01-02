2265b48ff1792d2a020059f5d0757a1ffe529538
MaciejFigat <60787137+MaciejFigat@users.noreply.github.com>
2022-01-02 19:28:00 +0100

    deployment tryout v4.21.3

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -41,9 +41,9 @@ if (process.env.NODE_ENV === 'production') {
 
 
 
-    app.use(express.static(path.join(__dirname, '/frontend/build')))
+    app.use(express.static(path.join(path.resolve(), '/frontend/build')))
     app.get('*', (req, res) =>
-        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+        res.sendFile(path.resolve('frontend', 'build', 'index.html'))
 
     )
 } else {
