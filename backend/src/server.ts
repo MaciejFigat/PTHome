bf2afe5b9f00592891e6d29787957d81d6b046e4
MaciejFigat <60787137+MaciejFigat@users.noreply.github.com>
2022-01-02 21:03:37 +0100

    deployment tryout v4.23

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -40,19 +40,19 @@ if (process.env.NODE_ENV === 'production') {
     //     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
     // )
 
-    app.use(express.static('frontend/build'));
+    // app.use(express.static('frontend/build'));
 
-    app.get('*', (req, res) => {
-        // res.sendFile(path.resolve(path.resolve(), 'frontend', 'build', 'index.html'));
-        res.sendFile(path.resolve('frontend', 'build', 'index.html'));
-    });
+    // app.get('*', (req, res) => {
+    //     res.sendFile(path.resolve(path.resolve(), 'frontend', 'build', 'index.html'));
 
-    //  app.use(express.static(path.join(path.resolve(), '/frontend/build')))
-    // app.get('*', (req, res) =>
-    //  res.sendFile(path.resolve('frontend', 'build', 'index.html'))
-    //  res.sendFile(path.resolve(path.resolve(), 'frontend', 'build', 'index.html'))
+    // });
 
-    // )
+    app.use(express.static(path.join(path.resolve(), '../../frontend/build')))
+    app.get('*', (req, res) =>
+
+        res.sendFile(path.resolve(path.resolve(), 'frontend', 'build', 'index.html'))
+
+    )
 } else {
     app.get('/', (req, res) => {
         res.send('API is running')
