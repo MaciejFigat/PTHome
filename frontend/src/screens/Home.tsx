2c20e45d44021a7c7bcd3bef91328ec9e1b42a2b
MaciejFigat <60787137+MaciejFigat@users.noreply.github.com>
2022-01-03 23:24:51 +0100

    Home screen restoration

--- frontend/src/screens/Home.tsx
+++ frontend/src/screens/Home.tsx
@@ -1,28 +1,28 @@
 import React from 'react'
-// import InfoSection from '../components/InfoSection/InfoSection'
-// import {
-//   homeData,
-//   homeDataTwo,
-//   homeDataThree,
-//   homeDataFour,
-//   homeDataFive,
-// } from '../data/homeData'
+import InfoSection from '../components/InfoSection/InfoSection'
+import {
+  homeData,
+  homeDataTwo,
+  homeDataThree,
+  homeDataFour,
+  homeDataFive,
+} from '../data/homeData'
 import Toast from '../components/Toast/Toast'
 
 const Home: React.FC = () => {
   return (
     <>
       <Toast option='registerUser' />
-      {/* <InfoSection data={homeData} variant='blue' imgStart></InfoSection> */}
-      {/* <InfoSection data={homeDataTwo} variant='primary'></InfoSection> */}
-      {/* <InfoSection
+      <InfoSection data={homeData} variant='blue' imgStart></InfoSection>
+      <InfoSection data={homeDataTwo} variant='primary'></InfoSection>
+      <InfoSection
         data={homeDataThree}
         variant='secondary'
         imgStart
-      ></InfoSection> */}
-      {/* <InfoSection data={homeDataFour} variant='tertiary'></InfoSection>
-       <InfoSection variant='secondary' data={homeDataFive}></InfoSection>
-      <InfoSection variant='primary' data={homeDataFive}></InfoSection> */}
+      ></InfoSection>
+      <InfoSection data={homeDataFour} variant='tertiary'></InfoSection>
+      <InfoSection variant='secondary' data={homeDataFive}></InfoSection>
+      <InfoSection variant='primary' data={homeDataFive}></InfoSection>
     </>
   )
 }
