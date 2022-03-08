84d54ec061e150083bccc577e6827e10a2804c51
MaciejFigat <60787137+MaciejFigat@users.noreply.github.com>
2022-03-08 23:51:13 +0100

    betterTemplates

--- backend/utilities/nodemailerTemplates/nodemailerTemplate.ts
+++ backend/utilities/nodemailerTemplates/nodemailerTemplate.ts
@@ -3,20 +3,27 @@ import fs from "fs";
 import ejs from "ejs";
 import { htmlToText } from "html-to-text";
 import juice from "juice";
-import settings from "../settings";
+// import settings from "../settings";
 
 const smtp = nodemailer.createTransport({
-    host: settings?.smtp?.host,
-    port: settings?.smtp?.port,
-    secure: process.env.NODE_ENV !== "development",
+    service: process.env.SERVICE,
     auth: {
-        user: settings?.smtp?.username,
-        pass: settings?.smtp?.password,
+        user: process.env.USER,
+        pass: process.env.PASS,
     },
 });
 
-export default ({ template: templateName, templateVars, ...restOfOptions }) => {
-    const templatePath = `lib/email/templates/${templateName}.html`;
+interface templateProps {
+    template: string
+    templateVars: {
+        emailAddress: string,
+        resetLink: string,
+    },
+}
+// .... not yet working 
+const sendTemplate: templateProps = ({ template: templateName, templateVars, ...restOfOptions }) => {
+    // export default ({ template: templateName, templateVars, ...restOfOptions }) => {
+    const templatePath = `../templates/${templateName}.html`;
     const options = {
         ...restOfOptions,
     };
@@ -33,3 +40,16 @@ export default ({ template: templateName, templateVars, ...restOfOptions }) => {
 
     return smtp.sendMail(options);
 };
+
+export default sendTemplate
+// to use the sendMail
+// await sendEmail({
+//     to: args.emailAddress,
+//     from: settings?.support?.email,
+//     subject: "Reset Your Password",
+//     template: "reset-password",
+//     templateVars: {
+//       emailAddress: args.emailAddress,
+//       resetLink,
+//     },
+//   });
