03b0345e4e4f2530fe5d6b70e4af1132c939e034
MaciejFigat <60787137+MaciejFigat@users.noreply.github.com>
2022-06-10 18:49:46 +0200

    not working version

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,4 @@
-import express, { request, Request, Response } from 'express'
+import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
 import morgan from 'morgan'
@@ -8,7 +8,7 @@ import articleRoutes from '../routes/articleRoutes'
 import fragmentRoutes from '../routes/fragmentRoutes'
 import connectDB from '../config/db'
 import { notFound, errorHandler } from '../middleware/errorMiddleware'
-import lexapiRoutes from '../routes/lexapiRoutes'
+// import lexapiRoutes from '../routes/lexapiRoutes'
 import axios from 'axios'
 
 dotenv.config()
@@ -29,33 +29,54 @@ app.use('/api/articles', articleRoutes)
 app.use('/api/fragments', fragmentRoutes)
 // app.use('/lexapi/', lexapiRoutes)
 
-//todo lex API
-// app.get('/lexapi', (req, res) => {
+//todo lex API search query
+
 app.get(`/lexapi/search/`, async (req: any, res: any, next) => {
-    // const {
-    //     searchquery
-    // } = req.body
-    // const {
-    //     searchquery
-    // } = req.queryreq.query.
+
     const searchquery = req.query.searchquery
-    // const searchquery = req.query.id
-    // const searchquery = 'test'
-    // axios.get(`https://null.turbo-lex.pl/search/?query=${searchquery}`)
+
     axios.get(`https://null.turbo-lex.pl/search/?query=${searchquery}`)
-
-
         .then(response => {
-            console.log(response.data.url);
-            console.log(response.data);
-            // console.log(res.json(JSON.parse(JSON.stringify(response.data))));
-            // res.json(JSON.parse(JSON.stringify(response.data)))
+            // console.log(response.data.url);
+            // console.log(response.data);
             res.json(JSON.parse(JSON.stringify(response.data)))
-
         })
         .catch(error => {
             console.log(error);
         });
+
+})
+// todo https://null.turbo-lex.pl/doc/26042463?query_doc=dochodowy&selected_doc=2
+app.get(`/lexapi/doc/:nr`, async (req: any, res: any, next) => {
+    // const query = 'podatek dochodowy'
+    // const selectedDoc = 2
+    // const docNumber = 26042463
+    // const { searchquery, selectedDoc, docNumber } = req.query.docQuery
+    // const { query, selectedDoc, docNumber } = req.query.searchquery
+    // const { query, selectedDoc, docNumber } = req.query.docQuery
+
+    // const searchquery = 'dochodowy'
+    // const selectedDoc = 2
+    // const docNumber = 26042463
+    // if (
+    //     typeof req.query.searchquery !== 'undefined'
+    // typeof query !== 'undefined' &&
+    // typeof selectedDoc !== 'undefined' &&
+    // typeof docNumber !== 'undefined'
+    // ) {
+    // axios.get(`https://null.turbo-lex.pl/doc/${docNumber}?query_doc=${query}&selected_doc=${selectedDoc}`)
+    axios.get(`https://null.turbo-lex.pl/doc/${req.query.nr}`)
+        .then(response => {
+            // console.log(response.data.url);
+            // console.log(response.data);
+            res.json(JSON.parse(JSON.stringify(response.data)))
+        })
+        .catch(error => {
+            console.log(error);
+        });
+    // } else {
+    //     return
+    // }
 
 })
 // ! Working example ----------- 
@@ -73,17 +94,7 @@ app.get(`/lexapi/search/`, async (req: any, res: any, next) => {
 
 // })
 // ! Working example End ----------
-//@ts-ignore
-// request(
-//     { url: 'https://null.turbo-lex.pl/' },
-//     (error: any, response: Response, body: string) => {
-//         if (error || response.statusCode !== 200) {
-//             return res.status(500).json({ type: 'error', message: error.message });
-//         }
 
-//         res.json(JSON.parse(body));
-//     }
-// )
 
 
 
@@ -152,11 +163,9 @@ app.use(errorHandler)
 const PORT = process.env.PORT || 5000
 
 
-
-
 app.listen(PORT, () => {
     console.log(
-        colors.yellow.bgCyan.bold(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+        colors.yellow.bgGreen.bold(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 
     )
 })
