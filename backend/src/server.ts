c09a5a174358f2c9bd7a98d9497fb3bd68197011
MaciejFigat <60787137+MaciejFigat@users.noreply.github.com>
2022-12-13 20:54:22 +0100

    server Access controll header change

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -20,7 +20,8 @@ app.use(express.json())
 
 //todo lex API
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
+    // res.header('Access-Control-Allow-Origin', '*');
+    res.header("Access-Control-Allow-Headers", "Content-Type");
     next();
 });
 
