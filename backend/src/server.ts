2e00baf063635191519a410ec6e90212db74857c
MaciejFigat <60787137+MaciejFigat@users.noreply.github.com>
2022-12-13 21:41:49 +0100

    auth and content-type headers

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -30,8 +30,7 @@ app.use((req, res, next) => {
 
 app.options("*", function (req, res) {
     res.header("Content-Type", "application/json");
-    res.header('Access-Control-Allow-Headers', 'Authorization');
-    res.header("Access-Control-Allow-Headers", "Content-Type");
+    res.header('Access-Control-Allow-Headers', 'Authorization, Content-Type')
     res.send();
 });
 
